b62734ac5b51255d37b654c08a3e4cfa89555a08
Antoine Guenet <aguenet@gmail.com>
2017-07-13 12:45:32 +0200

    Add create task on enter input.

--- js/views/AppView.js
+++ js/views/AppView.js
@@ -1,9 +1,15 @@
 var AppView = Backbone.View.extend ({
    
-   el: '#projectapp', 
+   el: '#projectapp',
+
+   events: {
+      'keypress #new-task': 'createOnEnter'
+    },
 
    // It's the first function called when this view is instantiated. 
    initialize: function() { 
+      this.$input = this.$('#new-task');
+
       tasks = new Tasks();
       this.listenTo(tasks, 'add', this.addTask);
    },
@@ -11,10 +17,23 @@ var AppView = Backbone.View.extend ({
    addTask: function( task ) {
       var view = new TaskView({ model: task });
       $('#tasks').append( view.render().el );
-   }
+   },
+
+   createOnEnter: function( event ) {
+      if ( event.which !== ENTER_KEY || !this.$input.val().trim() ) {
+         return;
+      }
+
+      var newTask = new Task();
+      newTask.set('title', this.$input.val());
+      tasks.add(newTask);
+
+      this.$input.val('');
+   },
 });
 
 var tasks;
+var ENTER_KEY = 13;
 
 $(function() {
    new AppView();
--- js/views/TaskView.js
+++ js/views/TaskView.js
@@ -6,7 +6,6 @@ var TaskView = Backbone.View.extend({
 
 	render: function() {
 		this.$el.html( this.template( this.model.attributes ) );
-		console.log(this);
 		return this;
     }
 });
