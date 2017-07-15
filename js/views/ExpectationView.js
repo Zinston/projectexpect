209ad200e97817e58df7be215e101d1047323c9c
Antoine Guenet <aguenet@gmail.com>
2017-07-15 19:16:34 +0200

    Add edit expectation.

--- js/views/ExpectationView.js
+++ js/views/ExpectationView.js
@@ -3,10 +3,16 @@ var ExpectationView = Backbone.View.extend({
 	className: 'expectation',
 
 	events: {
-      'click': 'completeExpectation'
+      'click #edit-expectation-btn': 'edit',
+      'keypress #edit-expectation': 'close',
+      'click #expectation-name': 'completeExpectation'
     },
 
 	template: _.template( $('#expectation-template').html() ),
+
+	initialize: function() {
+        this.listenTo(this.model, 'change', this.render);
+	},
 
 	render: function() {
 		this.$el.html( this.template( this.model.attributes ) );
@@ -16,5 +22,26 @@ var ExpectationView = Backbone.View.extend({
     completeExpectation: function(elem) {
     	this.model.toggle();
     	this.$el.toggleClass('complete');
+    },
+
+    close: function() {
+    	this.$edit = this.$('#edit-expectation');
+        if ( event.which !== ENTER_KEY || !this.$edit.val().trim() ) {
+            return;
+        }
+
+        this.$el.children('#expectation-name').css('display', 'block');
+        this.$el.children('#edit-expectation').css('display', 'none');
+        this.$el.children('#edit-expectation-btn').css('display', 'block');
+
+        this.model.set('title', this.$edit.val());
+    },
+
+    edit: function() {
+    	this.$el.children('#expectation-name').css('display', 'none');
+        this.$el.children('#edit-expectation').css('display', 'inline');
+        this.$el.children('#edit-expectation-btn').css('display', 'none');
+
+        this.$el.children('#edit-expectation').focus();
     }
 });
